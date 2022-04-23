468bce6688778ade79c64f0067cb50f7cf674a9b
shubham <52554694+Shubham-naik-ai@users.noreply.github.com>
2022-04-23 15:18:56 +0530

    Update application.py

--- application.py
+++ application.py
@@ -31,7 +31,6 @@ try:
     classifier =load_model('Final_model.h5')  #Load model
     emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']  # Emotion that will be predicted
 except Exception:
-	st.write("Error loading cascade classifiers1")
     st.write("Error loading cascade classifiers1")
     
     
