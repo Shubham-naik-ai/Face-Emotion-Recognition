b9783af669fd48fab1a1704a6bdf8e8718970285
shubham <52554694+Shubham-naik-ai@users.noreply.github.com>
2022-04-23 15:02:45 +0530

    Update application.py

--- application.py
+++ application.py
@@ -29,8 +29,8 @@ try:
     face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     classifier =load_model('Final_model.h5')  #Load model
     emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']  # Emotion that will be predicted
-#except Exception:
-    #st.write("Error loading cascade classifiers1")
+except Exception:
+    st.write("Error loading cascade classifiers1")
     
     
 class VideoTransformer(VideoTransformerBase):
