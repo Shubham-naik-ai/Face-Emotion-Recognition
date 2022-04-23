83798973934d3602252b44d87b1d104f6a17dd52
shubham <52554694+Shubham-naik-ai@users.noreply.github.com>
2022-04-23 15:19:42 +0530

    Update application.py

--- application.py
+++ application.py
@@ -26,7 +26,6 @@ except ImportError:
 
 # Loading pre-trained parameters for the cascade classifier
 try:
-    st.write("Error loading cascade classifiers1")
     face_classifier = cv2.CascadeClassifier(cv2.samples.findFile(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'))
     classifier =load_model('Final_model.h5')  #Load model
     emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']  # Emotion that will be predicted
