05cfcc5e6efc166c6b9db3dc96117597f66f414a
shubham <52554694+Shubham-naik-ai@users.noreply.github.com>
2022-04-23 13:46:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -366,7 +366,9 @@ def main():
     elif choice=="Introduction":
          html_temp = """
     <body style="background-color:red;">
-    <div style="background-image: url('https://i1.wp.com/thedatascientist.com/wp-content/uploads/2015/12/cool_neural_network.jpeg?fit=1500%2C670&ssl=1');padding:150px">
+    <div style="background-image: url('https://i1.wp.com/thedatascientist.com/wp-content/uploads/2015/12/cool_neural_network.jpeg?fit=1500%2C670&ssl=1');
+    background-size: cover;padding:150px">
+    
     <h2 style="color:white;text-align:center;">YOUR EMOTION REFLECTS YOUR PERSONALITY.</h2>
     <h2 style="color:white;text-align:center;">To Know your emotion proceed to Home from the side bar.</h2>
     </div>
